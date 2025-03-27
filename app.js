3d1afae0d5173f4b93a185bd4d4462426eb2e89b
Jwe891116 <2005112848.ub.edu.bz>
2025-03-27 13:44:08 -0600

    Updated

--- app.js
+++ app.js
@@ -9,29 +9,17 @@ app.use(express.static(path.join(process.cwd(), "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(process.cwd(), "views"));
 
-
-// const loggingMiddleware = (req, res, next) => {
-//   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-//   next();
-// };
-
-// app.use(loggingMiddleware);
-
 app.use('/', guestbookRoutes);
-
-// app.use((req, res) => {
-//      res.status(404).send('Oops! The page you are looking for does not exist.');
-// });
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Oops! The page you are looking for does not exist.');
-});
 
 app.use((req, res, next) => {
   const timestamp= new Date().toISOString();
   console.log(`[${timestamp}] ${req.method} ${req.url}`);
   next();
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Oops! The page you are looking for does not exist.');
 });
 
 const PORT = 3000;
